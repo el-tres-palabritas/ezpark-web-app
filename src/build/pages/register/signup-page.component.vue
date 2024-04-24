5ea22c6d38ea66fd9d3857e928c7a0191df58a18
Gareth <sebastianmv2019@gmail.com>
2024-04-24 16:15:01 -0500

    feat(maq-signup): added signup page and component

---
+++ src/build/pages/register/signup-page.component.vue
@@ -0,0 +1,16 @@
+<script setup>
+import CompSignup from "@/public/components/signup.component.vue";
+</script>
+
+<template>
+  <header>
+    <h1>Sign Up</h1>
+  </header>
+  <main>
+    <comp-signup/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
