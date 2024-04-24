6f6ac19c418ea7342b4b4dbc84cbf7abbb8bcbeb
Gareth <sebastianmv2019@gmail.com>
2024-04-24 15:42:22 -0500

    feat(maq-signin): added signin page an component

---
+++ src/public/components/signin.component.vue
@@ -0,0 +1,109 @@
+<script setup>
+import PvInputText from "primevue/inputtext";
+import PvPassword from "primevue/password";
+import PvButton from "primevue/button";
+import {ref} from "vue";
+
+const value1 = ref(null);
+const value2 = ref(null);
+</script>
+
+<template>
+  <section class='form-container'>
+    <form action="" method="get">
+      <div class="form-item">
+        <div class="logo-container">
+          <img alt="Logo" class="logo" src="../../assets/logo.svg">
+        </div>
+        <h1 class="title">Sign In</h1>
+      </div>
+      <div class="form-item">
+        <label class="label" for="email">E-mail</label>
+        <pv-input-text id="email" v-model="value1" aria-describedby="email-help"/>
+      </div>
+      <div class="form-item">
+        <label class="label" for="password">Password</label>
+        <pv-password id="password" v-model="value2" :feedback="false" input-style="width: 100%; background-color: #D9D9D9; color: black;" toogleMask/>
+      </div>
+      <div class="form-item">
+        <pv-button id="submit" label="Login"/>
+      </div>
+      <div class="forget-password">
+        <h3 style="margin: 0;"><a style="color: black;" href="">Forgot password?</a></h3>
+      </div>
+      <div class="form-item">
+        <p>Don't have an account yet? <a style="color: #EF6C42;" href="">Sign up</a></p>
+      </div>
+    </form>
+  </section>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap');
+
+a {
+  text-decoration: none;
+}
+
+form {
+  display: flex;
+  align-items: center;
+  background-color: white;
+  flex-direction: column;
+  font-family: "Mulish", "Inter var", sans-serif;
+
+  padding: 20px;
+  width: 500px;
+  border-radius: 20px;
+}
+
+.title {
+  color: black;
+  margin: 10px 0 0 0;
+}
+
+.logo {
+  width: 160px;
+}
+
+
+.form-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: inherit ;
+
+  margin-bottom: 20px;
+  color: black;
+  width: 80%;
+}
+
+.label {
+  display: inline-block;
+
+  width: 100%
+}
+
+#email, #password {
+  background-color: #D9D9D9;
+  color: black;
+  width: 100%;
+}
+
+.forget-password {
+  display: inherit;
+  justify-content: flex-end;
+  font-family: inherit;
+
+  width: 80%;
+}
+
+#submit {
+  width: 100%;
+  font-family: inherit;
+  background-color: #EF6C42;
+  border: #D9D9D9 solid 2px;
+}
+
+</style>
---
+++ src/public/pages/login/login-page.component.vue
@@ -0,0 +1,23 @@
+<script setup>
+import CompSignin from "@/public/components/signin.component.vue";
+</script>
+
+<template>
+  <header>
+    <h1>Sign Up</h1>
+  </header>
+  <main>
+    <comp-signin/>
+  </main>
+</template>
+
+<style scoped>
+body {
+  background-color: #D9D9D9;
+}
+
+main {
+  display: flex;
+  justify-content: center;
+}
+</style>
