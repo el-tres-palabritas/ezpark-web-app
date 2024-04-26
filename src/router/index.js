2c4d4b4f56f42512014ba9aa66a0e3be119dc2c9
Chivicita <81532162+Chivicta@users.noreply.github.com>
2024-04-26 01:31:25 -0500

    fix: fixed login route

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginPage from 'src/public/pages/login/login-page.component.vue'
+import LogInPage from '@/public/pages/login/login-page.component.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: '/login',
-      name: LoginPage
+      component: LogInPage
     }
   ]
 })
