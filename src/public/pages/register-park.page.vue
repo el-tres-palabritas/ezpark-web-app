0c3c8db89df064b2969f93c3e056a8349c96c211
SgtGarro <marcelogarro137@gmail.com>
2024-05-03 18:36:56 -0500

    feat (TA-11): create layout for rent your parking

--- src/public/pages/register-park.page.vue
+++ src/public/pages/register-park.page.vue
@@ -1,13 +1,160 @@
 <script>
-import BaseLayout from '../layout/base.layout.vue'
-
 export default {
-  components: { BaseLayout },
   name: 'register-park-page'
 }
 </script>
+
+<script setup>
+import VBaseLayout from '@/public/layout/base.layout.vue'
+import VGoogleAutocomplete from '@/public/components/google-autocomplete.component.vue'
+import VGoogleMap from '../components/google-map.component.vue'
+import PvInputText from 'primevue/inputtext'
+import PvInputMask from 'primevue/inputmask'
+import PvButton from 'primevue/button'
+
+import { reactive, ref } from 'vue'
+
+const MAP_DEFAULT_CENTER = { lat: -12.1061161, lng: -77.026921 }
+const MAP_DEFAULT_ZOOM = 18
+
+const map = ref(null)
+
+const formValue = reactive({})
+
+/** @param {google.maps.places.PlaceResult} place */
+function handleAutocompletePlaceChanged(place) {
+  const { location } = place.geometry
+  if (!location) return
+  map.value.panTo(location)
+  map.value.setZoom(18)
+}
+</script>
+
 <template>
-  <base-layout>
-    <h1>Register your park</h1>
-  </base-layout>
+  <v-base-layout>
+    <section class="register-section">
+      <h2 class="register-title">Rent your parking</h2>
+
+      <form @submit.prevent>
+        <div class="register-form-section--top">
+          <div class="register-form-field register-form-field--address">
+            <label for="address">Exact address of the space to rent:</label>
+            <v-google-autocomplete @placeChanged="handleAutocompletePlaceChanged" />
+          </div>
+          <div class="register-form-field register-form-field--spaces">
+            <label for="address">Parking spaces available</label>
+            <pv-input-text />
+          </div>
+        </div>
+        <div class="map-container">
+          <v-google-map :center="MAP_DEFAULT_CENTER" :zoom="MAP_DEFAULT_ZOOM" v-model:map="map" />
+        </div>
+        <div class="register-form-section--bottom">
+          <div class="register-form-additional-info">
+            <div class="register-form-field">
+              <label for="length">Length (m):</label>
+              <pv-input-text />
+            </div>
+            <div class="register-form-field">
+              <label for="width">Width (m):</label>
+              <pv-input-text />
+            </div>
+            <div class="register-form-field">
+              <label for="address">Height (m):</label>
+              <pv-input-text />
+            </div>
+            <div class="register-form-field">
+              <label for="startTime">Start time:</label>
+              <pv-input-mask mask="99:99" />
+            </div>
+            <div class="register-form-field">
+              <label for="endTime">End time</label>
+              <pv-input-mask mask="99:99" />
+            </div>
+            <div class="register-form-field">
+              <label for="phone">Phone number</label>
+              <pv-input-mask mask="999-999-999" />
+            </div>
+          </div>
+          <div class="register-form-cta">
+            <div class="register-form-field register-form-field--fare">
+              <label for="fare">Price per hour (includes commission)</label>
+              <pv-input-text />
+            </div>
+            <div class="register-form-field register-form-field--description">
+              <label for="description">Description</label>
+              <pv-input-text />
+            </div>
+            <pv-button class="register-form-post-btn" label="Post parking" severity="contrast" />
+          </div>
+        </div>
+      </form>
+    </section>
+  </v-base-layout>
 </template>
+
+<style scoped>
+.register-section {
+  background: #e7efff;
+  padding: 2rem 2rem;
+  border-radius: 20px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.15);
+}
+.register-title {
+  font-size: 2.5rem;
+  font-weight: 700;
+  font-family: 'Rubik', sans-serif;
+  margin-block: 0 0.75rem;
+}
+.register-form-section--top {
+  display: flex;
+  gap: 1.5rem;
+  margin-bottom: 1.25rem;
+}
+.register-form-field {
+  display: flex;
+  flex-direction: column;
+}
+.register-form-field--address {
+  width: 100%;
+}
+.register-form-field--spaces {
+  flex-shrink: 0;
+  width: 15rem;
+}
+.register-form-additional-info .register-form-field :deep(.p-inputtext) {
+  width: 100%;
+}
+.register-form-field label {
+  margin-bottom: 0.5rem;
+}
+.map-container {
+  width: 100%;
+  aspect-ratio: 3/1;
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.15);
+}
+.register-form-additional-info {
+  display: flex;
+  gap: 1rem;
+  margin-top: 1.25rem;
+}
+.register-form-cta {
+  display: flex;
+  gap: 1.25rem;
+  margin-top: 1.5rem;
+}
+.register-form-field--fare {
+  width: 20rem;
+}
+.register-form-field--description {
+  width: 100%;
+}
+.register-form-post-btn {
+  text-wrap: nowrap;
+  flex-shrink: 0;
+  height: fit-content;
+  margin-block: auto;
+}
+</style>
